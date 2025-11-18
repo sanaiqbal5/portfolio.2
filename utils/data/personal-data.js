2173cdb9676d5dcda97924457f4e59c5877afadb
sanaiqbal5 <164328129+sanaiqbal5@users.noreply.github.com>
2025-11-18 22:20:41 +0500

    update

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,6 @@ export const personalData = {
   designation: "Software Engineer/ Full Stack Developer",
   description: "My name is Sana Iqbal. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'sana.iqbal.siqbal@gmail.com',
-  phone: '+923166505468',
   address: 'Lahore, Pakistan ',
   github: 'https://github.com/sanaiqbal-siqbal/',
   facebook: 'https://www.facebook.com/',
